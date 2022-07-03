e96921db5d0a9b620c5a077be189dac067b904e7
Rahmat <oktrifianto@gmail.com>
2022-07-03 22:45:09 +0700

    Update: remove comment in `countSubstring`

--- Python/CountSubstring/CountSubstring.py
+++ Python/CountSubstring/CountSubstring.py
@@ -7,14 +7,14 @@ class Solution:
   def count_substring(mainString, subString):
     count = 0
     for i in range(len(mainString)):
-      print(mainString[i])
+      # print(mainString[i])      # only debugging
       if mainString[i:].startswith(subString):    # like slice() method in JS
         # print(mainString[i:])   # only debugging
         count += 1
     return count
 
-print(Solution.count_substring('ABABABC', 'AB')) # ABABABC, ABABC, ABC
+print(Solution.count_substring('ABABABC', 'AB')) # 3 => ABABABC, ABABC, ABC
 print('---------------')
-print(Solution.count_substring('ABABABC', 'ABA')) # ABABABC, ABABC
+print(Solution.count_substring('ABABABC', 'ABA')) # 2 => ABABABC, ABABC
 print('---------------')
-print(Solution.count_substring('ABCDCDC', 'CDC')) # CDCDC, CDC
+print(Solution.count_substring('ABCDCDC', 'CDC')) # 2 => CDCDC, CDC
